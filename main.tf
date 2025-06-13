562be67b4a8fd1421f817cd6cc868392664ba54c
Harish Nakireddy <harishn889@gmail.com>
2025-06-13 15:04:22 +0530

    added the policy

--- main.tf
+++ main.tf
@@ -10,3 +10,18 @@ resource "aws_iam_user" "admin_user" {
         Description = "Admin"
     }
 }
+
+resource "aws_iam_polilcy" "admin_policy" {
+    name = "AdminPolicy"
+    description = "Admin policy for full access"
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Action = "*"
+                Resource = "*"
+            }
+        ]
+    })
+}
