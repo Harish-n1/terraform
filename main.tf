edd8ccfd53332c0f758f98a94e00b14549c6b127
Harish Nakireddy <harishn889@gmail.com>
2025-06-14 15:50:12 +0530

    initial

--- main.tf
+++ main.tf
@@ -1,24 +1,42 @@
-provider "aws" {
-    region = ""
-    access_key = ""
-    secret_key = ""
+provider "azurerm" {
+  features {}
 }
 
-resource "aws_iam_user" "admin_user" {
-    name = "hari"
-    tags = {
-        Description = "devops user"
-    }
+# Create a Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "devops-rg"
+  location = "East US"
 }
 
-resource "aws_iam_polilcy" "admin_policy" {
-    name = "AdminPolicy"
-    description = "Admin policy for full access"
-    policy = file("admin_policy.json")
+# Create an AKS cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "devops-aks-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "devops-aks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    dns_service_ip = "10.2.0.10"
+    service_cidr   = "10.2.0.0/24"
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
 }
 
-resource "aws_iam_policy_attachment" "hari-admin-access" {
-    name       = "hari-admin-access"
-    users      = aws_iam_user.admin_user.name
-    policy_arn = aws_iam_polilcy.admin_policy.arn
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
 }
